282a2b55bf19b6c79fb1e92303cc31dea552c66f
Miri <m05832773312GMAIL.COM>
2022-02-23 18:24:54 +0200

    nech

--- src/app/modules/patient/patient.module.ts
+++ src/app/modules/patient/patient.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientComponent } from './patient/patient.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AppModule } from 'src/app/app.module';
+import {SliderModule} from 'primeng/slider'
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {SlideMenuModule} from 'primeng/slidemenu';
+import {ToolbarModule} from 'primeng/toolbar';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview/tabview';
+// import { SharedModule, PanelModule } from 'primeng/primeng';
+
 
 const ROUTES:Routes=[
 {path:"",component:PatientComponent}
@@ -13,7 +22,13 @@ const ROUTES:Routes=[
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(ROUTES)
-  ]
+    RouterModule.forChild(ROUTES),
+    SliderModule,
+    SlideMenuModule,
+    ContextMenuModule,
+    ToolbarModule,
+    TabViewModule
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class PatientModule { }
--- src/app/modules/patient/patient/patient.component.ts
+++ src/app/modules/patient/patient/patient.component.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuItem} from 'primeng/api';
+import { PatientDTO } from 'src/app/models/patientDTO.model';
+
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
   styleUrls: ['./patient.component.css']
 })
 export class PatientComponent implements OnInit {
-
-  constructor() { }     
-
+user!:PatientDTO
+items: MenuItem[] | undefined;
+  constructor() { 
+    var u=sessionStorage.getItem("user");
+    if(u)
+      this.user=JSON.parse(u); 
+  }   
+  
   ngOnInit(): void {
+    this.items = [
+      {
+          label: 'Update',
+          icon: 'pi pi-refresh'
+      },
+      {
+          label: 'Delete',
+          icon: 'pi pi-times'
+      },
+      {
+          label: 'Angular Website',
+          icon: 'pi pi-external-link',
+          url: 'http://angular.io'
+      },
+      {
+          label: 'Router',
+          icon: 'pi pi-upload',
+          routerLink: '/fileupload'
+      }
+  ];
+ 
+ 
+  }  
 
-  }
 
+
+
+
+
+
+ 
 }
