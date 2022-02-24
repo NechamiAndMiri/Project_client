adf3f1921264d7b7b182df25eef76e414046ffb9
325192797 <325192797@PC5-28>
2022-02-24 13:39:30 +0200

    services

---
+++ src/app/services/lesson.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { Lesson } from '../models/lesson.model';
+import { WordGivenToPractice } from '../models/wordGivenToPractice.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class LessonService {
+
+  constructor(private _http:HttpClient) { }
+
+
+  getLessonsByPatient(patientId:number ):Observable<Lesson[]>{
+    
+    return this._http.get<Lesson[]>("api/User/"+patientId);
+}
+
+getWordsToLesson(lessonId:number):Observable<WordGivenToPractice[]>{
+  return this._http.get<WordGivenToPractice[]>("api/User/"+lessonId);
+}
+
+}
